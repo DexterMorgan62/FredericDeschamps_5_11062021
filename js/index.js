95dff44e2a02ee3034fd00d11da37181ab0f9eb0
Deschamps <deschfred62@yahoo.fr>
2021-06-17 23:22:49 +0200

    index.js

--- js/index.js
+++ js/index.js
@@ -1,24 +1,14 @@
-const affichageName = document.querySelector(#name);
-console.log(affichageName)
-function askHello() {
     fetch("http://localhost:3000/api/cameras")
     .then(function(res) {
       if (res.ok) {
         return res.json();
-        console.log(res);
       }
     })
     .then(function(value) {
       console.log(value);
-      const txtName = value[0].name;
-      affichageName.innerHTML = txtName;
-          
+
+      
     })
     .catch(function(err) {
       // Une erreur est survenue
     });
-  }
-  
-  document
-    .getElementById("ask-hello")
-    .addEventListener("click", askHello);
