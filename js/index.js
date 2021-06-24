a32684b033f47b857a07e17dadf82900895dbe6e
Deschamps <deschfred62@yahoo.fr>
2021-06-24 19:35:46 +0200

    css and js

--- js/index.js
+++ js/index.js
@@ -25,9 +25,9 @@ const showThumbnails = cameras => {
     boxCameras.appendChild(vignetteCameras)
     console.log(vignetteCameras);
     const nameVignette = camera.name
-    vignetteCameras.className = "vignette-cameras" + "-" + nameVignette
+    vignetteCameras.className = "vignette-cameras"
     
-    const cameraName = document.createElement("name.cameras")
+    const cameraName = document.createElement("h3")
     cameraName.innerHTML = camera.name
     vignetteCameras.appendChild(cameraName)
 
@@ -35,14 +35,17 @@ const showThumbnails = cameras => {
     cameraDescription.innerHTML = camera.description
     vignetteCameras.appendChild(cameraDescription)
 
-    const cameraPrice = document.createElement("price.cameras")
+    const cameraPrice = document.createElement("p")
     cameraPrice.innerHTML = camera.price
     vignetteCameras.appendChild(cameraPrice)
 
-    /*const cameraImage = document.createElement("img")
-    
+    const cameraImage = document.createElement("img")
+    cameraImage.src=camera.imageUrl 
     vignetteCameras.appendChild(cameraImage)
-  */
+  
+    const cameraLink = document.createElement("a")
+    cameraLink.href= 'product.html?id=' + camera._id
+    vignetteCameras.appendChild(cameraLink)
     
   }
 
